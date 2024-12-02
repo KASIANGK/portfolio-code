6bfbafe01f6d6f692c42b83d3867c68ebada89af
kiss_my_fire <ngk.kasia@gmail.com>
2024-12-02 20:19:37 +0100

    vite.config.js 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Remplace "portfolio" par le nom de ton dépôt si nécessaire
+  base: '/', // Remplace "portfolio" par le nom de ton dépôt si nécessaire
 })
